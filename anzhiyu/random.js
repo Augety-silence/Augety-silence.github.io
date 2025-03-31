9df34bd46ed7884d4121c1fef93757007d58eb9f
Augety.silence <2137979327@qq.com>
2025-03-31 23:40:07 +0800

    Site updated: 2025-03-31 23:40:07

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/20/pre-zhzx/","2025/03/06/fayangao/","2025/03/20/presentation/"];function toRandomPost(){
+var posts=["2025/03/31/birthday2024/","2025/03/06/fayangao/","2025/03/20/pre-zhzx/","2025/03/20/presentation/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
