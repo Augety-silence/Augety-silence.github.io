6da0fbb822b47d3646806335fa6e2fe7fe008436
Augety.silence <2137979327@qq.com>
2025-03-31 23:53:28 +0800

    Site updated: 2025-03-31 23:53:27

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/06/fayangao/","2025/04/01/birthday2025/","2025/03/20/presentation/","2025/03/20/pre-zhzx/"];function toRandomPost(){
+var posts=["2025/04/01/birthday2025/","2025/03/06/fayangao/","2025/03/20/presentation/","2025/03/20/pre-zhzx/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
